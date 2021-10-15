cf7c8b7ca148d90ece018be6396a124fbac4785f
Piotr Rybka <piotr.rybka82@gmail.com>
2021-10-16 00:13:11 +0200

    Cleaned usings

--- TTO.ToDoApp.Tests.Integration/Application/AddTodoHandlerTests.cs
+++ TTO.ToDoApp.Tests.Integration/Application/AddTodoHandlerTests.cs
@@ -1,14 +1,6 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
-using Xunit;
-using TTO.TodoApp.Infrastructure.Ef;
 using TTO.TodoApp.Application;
+using Xunit;
 
 namespace TTO.ToDoApp.Tests.Integration.Application
 {
--- TTO.ToDoApp.Tests.Integration/Application/GetTodoHandlerTests.cs
+++ TTO.ToDoApp.Tests.Integration/Application/GetTodoHandlerTests.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentAssertions;
 using TTO.TodoApp.Application;
-using Xunit;
-using FluentAssertions;
-using System.Runtime.CompilerServices;
-using TTO.TodoApp.Infrastructure.Ef;
 using TTO.TodoApp.Domain;
+using Xunit;
 
 namespace TTO.ToDoApp.Tests.Integration.Application
 {
